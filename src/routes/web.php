9a7ecb5288d88a73f07ce7b6bf24c5fe151deae5
油井真 <yuimakoto@MacBook-Pro.local>
2024-08-13 19:28:33 +0900

    Route・Viewのリファクタリング

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,3 +22,5 @@ Route::get('/todo', function () {
 Route::get('/todo', 'TodoController@index');
 //section11追加内容
 Route::get('/todo/create', 'TodoController@create');
+//section12追加内容
+Route::get('/todo/create', 'TodoController@create')->name('todo.create');

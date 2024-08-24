4fd83c7c4b38d398d0f11a3a28e9966e8675a0ed
油井真 <yuimakoto@MacBook-Pro.local>
2024-08-24 23:48:14 +0900

    ToDo論理削除機能の実装

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+//section22追加内容
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+    //section22追加内容
+    use SoftDeletes;
+    
     //section8追加内容
     protected $table = 'todos';
 
